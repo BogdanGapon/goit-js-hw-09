ac29a023a0d9d57956e1f8220d99a56b97fe327c
Bogdan <bogdangaponenko1996@gmail.com>
2023-02-09 09:51:44 -0600

    fixed problem with disabled btn in 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,8 +5,10 @@ function getRandomHexColor() {
 const body = document.body;
 const btnOpen = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
+
 const DELAY = 1000;
 let changingColor = null;
+btnStop.disabled = true;
 
 body.addEventListener('click', onIntervalChangingBodyColor);
 function onIntervalChangingBodyColor(evt) {
