c0f53e74caf762ff77c73bdc93358a17e404149c
Bogdan <bogdangaponenko1996@gmail.com>
2023-02-10 01:11:04 -0600

    resolve 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  const delay = +evt.target.elements.delay.value;
+  const delayStep = Number(evt.target.elements.step.value);
+  const amount = Number(evt.target.elements.amount.value);
+  let firstDelay = delay - delayStep;
+  for (let i = 1; i <= amount; i += 1) {
+    let position = i;
+    firstDelay += delayStep;
+
+    createPromise(position, firstDelay)
+      .then(resolve => {
+        Notify.success(resolve);
+      })
+      .catch(reject => {
+        Notify.failure(reject);
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
